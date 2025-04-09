045689f4f14c753d8674a2d470ffe07a2416a91d
PhuHoang07 <duongphu1207@gmail.com>
2025-04-09 23:55:17 +0700

    + update game

--- Test/Assets/Script/Balloon.cs
+++ Test/Assets/Script/Balloon.cs
@@ -5,12 +5,46 @@ public class Balloon : MonoBehaviour
 {
     public int scoreValue = 10;
     private Animator animator;
+    public AstraInputController inputController;
+    private bool isHandOver = false;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+
+        if (inputController == null)
+        {
+            inputController = FindObjectOfType<AstraInputController>();
+        }
+
+        if (inputController != null)
+        {
+            inputController.OnClickEvent.AddListener(HandleClick);
+        }
     }
+
+    private void OnDestroy()
+    {
+        if (inputController != null)
+        {
+            inputController.OnClickEvent.RemoveListener(HandleClick);
+        }
+    }
+
+    private void HandleClick()
+    {
+        if (isHandOver)
+        {
+            PopBalloon();
+        }
+    }
+
     private void OnMouseDown()
+    {
+        PopBalloon();
+    }
+
+    private void PopBalloon()
     {
         GameManager.instance.AddScore(scoreValue);
 
@@ -18,8 +52,24 @@ public class Balloon : MonoBehaviour
         {
             animator.Play("redPop");
         }
-        
 
         Destroy(gameObject, 0.3f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"Trigger enter: {other.name}");
+        if (other.CompareTag("Hand"))
+        {
+            isHandOver = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            isHandOver = false;
+        }
     }
 }
